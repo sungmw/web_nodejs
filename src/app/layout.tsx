0e2729e324c9e7a2aa4a27e9c4735a4bafaac268
Minwook Seong <sungmw1004@gmail.com>
2024-06-18 23:40:26 +0900

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gradient-to-b from-blue-800 to-blue-400`}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,38 @@
-import Image from 'next/image'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
+import Image from 'next/image';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 export default function Home() {
-  const name = 'JIHOON'
-  const result = 1 + 2
+  const name = 'JIHOON';
+  const result = 1 + 2;
+
   return (
-    <main>
+    <main className="min-h-screen">
       <div className="font-mono bg-blue-900 text-blue-300">
         <Navbar />
       </div>
-      <div className="font-mono bg-blue-900 text-blue-300 text-center mt-1 p-4">
-        <h1 className="text-3xl font-bold">Main Page! Wa!!!!</h1>
+      <div className="font-mono bg-blue-800 text-white text-center mt-1 p-6">
+        <h1 className="text-4xl font-bold">Index Page Home</h1>
       </div>
-      <div className="font-mono bg-blue-600 text-blue-300 text-center m-4">
-        <p>{name}, WELCOME!</p>
-        <p>This page is made for node js, next js, tailwind.</p>
+      <div className="font-mono bg-blue-700 text-white text-center m-4 p-6 rounded-lg shadow-lg">
+        <p className="text-2xl">{name}, WELCOME!</p>
+        <p>이 페이지는 node.js다</p>
       </div>
-      <div className="font-mono bg-blue-600 text-blue-300 text-center m-4">
-        <p>1 + 2 = {result}</p>
-        <p>
-          node js 안에 슬라이드쇼와 같은 기능을 어떻게 구현할 지 모르겠어요.
-        </p>
-        <p>
-          나중에 방학때 시간이 된다면 node js 를 사용하는 법에 대해서 더
-          면밀하게 공부해보고 싶어요.
-        </p>
+      <div className="font-mono bg-blue-600 text-white text-center m-4 p-6 rounded-lg shadow-lg">
+        <p className="text-2xl">2023 + 2024 = {result}</p>
+        <p>node js는 뭘까응..</p>
+        <p>안녕하세요 성민욱입니다</p>
       </div>
-      <div className="font-mono bg-blue-600 text-blue-300 text-center m-4 flex justify-center">
+      <div className="font-mono bg-blue-500 text-white text-center m-4 p-6 rounded-lg shadow-lg flex justify-center">
         <Image
           src="/image_47.jpg"
           alt="minwook.jpg"
           width="500"
           height="300"
-        ></Image>
+          className="rounded-lg shadow-lg"
+        />
       </div>
       <Footer />
     </main>
-  )
+  );
 }
